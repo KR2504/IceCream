9a39cf2ff67d449686a900624bdd6e2106e3ec00
Adelina Denysova <adochkadenisovaa@gmail.com>
2022-01-19 18:07:31 +0200

    add partials for menu, fix menu, fix buttons

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
-    let nav_list = document.getElementsByClassName('header__table-menu')[0];
+    let nav_list = document.getElementsByClassName('menu')[0];
     let menu_btn = document.getElementsByClassName('header__button-menu')[0];
-    let button_close = document.getElementsByClassName('header__button-close')[0];
+    let button_close = document.getElementsByClassName('menu__button-close')[0];
 
     menu_btn.addEventListener('click', function(){
         nav_list.classList.toggle('is-open');
